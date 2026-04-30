187e03025577d02cced4318f4b8305b9c6f95f40
Sir Tai <eddhernan69@gmail.com>
2026-04-30 16:21:01 -0700

    Update spanish.js

--- decks/spanish.js
+++ decks/spanish.js
@@ -7,8 +7,8 @@ const spanishDecks = {
                 Greetings: {
                     type: "deck",
                     cards: [
-                        { front: "Hola", back: "Hello" },
-                        { front: "Gracias", back: "Thank you" }
+                        { front: "Hola", back: "Hello", break: "O-La", interval: 0, repetitions: 0, easeFactor: 2.5, due: Date.now()  },
+                        { front: "Gracias", back: "Thank you", break: "Gra-Cia", interval: 0, repetitions: 0, easeFactor: 2.5, due: Date.now()  }
                     ]
                 }
             }
